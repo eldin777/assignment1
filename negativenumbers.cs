579fce814e9b882b2fa132bbcd7d49cc91c9c608
eldin777 <69908631+eldin777@users.noreply.github.com>
2022-08-24 00:11:42 +0530

    Add files via upload

---
+++ negativenumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class negativenumbers
+    {
+        public static void Main()
+        {
+            int[] arr = { 12, -84, 2, -78, -54, 37 };
+            int i, n;
+            n = arr.Length;
+            for (i = 1; i < n; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.Write($"{arr[i]}");
+                }
+
+            }
+        }
+    }
+}
