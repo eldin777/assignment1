45ce807ce443f5e45ede545955e9cc7ffecfe917
eldin777 <69908631+eldin777@users.noreply.github.com>
2022-08-23 06:02:15 -0700

    Add files via upload

---
+++ Small1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Small
+    {
+        public static void minimum(int a1,int b1,out int min1)
+        {
+            if (a1 < b1)
+            {
+                min1 = a1;
+            }
+            else
+            {
+                min1 = b1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("First number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int min;
+            minimum(a, b, out min);
+          
+        }
+    }
+}
---
+++ product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class product
+    {
+
+        public static int Multiply(int a1, int b1, int c1)
+        {
+            return a1 * b1 * c1;
+        }
+        public static void Main()
+        {
+            int a, b, c;
+            Console.Write("First number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Third number");
+            c = Convert.ToInt32(Console.ReadLine());
+            int product = Multiply(a, b, c);
+            Console.WriteLine($"Product of three number is {product}");
+        }
+    }
+}
